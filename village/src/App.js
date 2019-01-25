63735ded550de6f2ca28f20c9ca936e72e4af5c5
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2019-01-25 11:22:23 -0600

    Added ability to delete a smurf along with a delete button

--- village/src/App.js
+++ village/src/App.js
@@ -36,6 +36,15 @@ class App extends Component {
       })
       .catch(err => console.log(err.response));
   }
+
+  deleteSmurf = (e, id) => {
+    e.preventDefault();
+    console.log("running"+id)
+    axios 
+      .delete(`http://localhost:3333/smurfs/${id}`)
+      .then(res => { this.setState({ smurfs: res.data })})
+      .catch(err => console.log(err));
+  }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
   // You'll need to make sure you have the right properties on state and pass them down to props.
@@ -49,7 +58,7 @@ class App extends Component {
             <NavLink to ="/">Home</NavLink>
           </div>
         </nav>
-        <Route exact path = "/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route exact path = "/" render={props => <Smurfs {...props} smurfs={this.state.smurfs} deleteSmurf={this.deleteSmurf} />} />
         <Route path ="/smurf-form" render={props => <SmurfForm {...props} createSmurf={this.createSmurf}/>} />
       </div>
     );
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -6,6 +6,9 @@ const Smurf = props => {
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
+      <form onSubmit={(e) => props.deleteSmurf(e, props.id)}>
+        <button>Delete</button>
+      </form>
     </div>
   );
 };
